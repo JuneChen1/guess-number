3987f523d90f0d2324f83a16b329fc474713d29d
JuneChen1 <june850713@gmail.com>
2025-11-25 03:24:30 +0000

    Updated the rule of user input

--- guess-number.py
+++ guess-number.py
@@ -6,7 +6,7 @@ B = 0
 
 while A != 4:
   num = input("請輸入四個數字：")
-  while len(num) != 4 or len(set(num)) != 4:
+  while num.isdigit() == False or len(num) != 4 or len(set(num)) != 4:
     num = input("請輸入四個數字，且數字勿重複：")
   
   numList = list(map(int, list(num)))
